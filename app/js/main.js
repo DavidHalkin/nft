c71c9d36c185aaa80a133ae47ba800d3e3768653
Halkin David <halkin.david@gmail.com>
2022-01-17 17:39:45 +0200

    page markup

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,38 @@
 $(document).ready(function(){
     $('.owl_carousel_js').owlCarousel({
-        margin:30,
+        responsiveClass:true,
+         dots:false,
+        autoWidth:true,
         responsiveClass:true,
         responsive:{
             0:{
-                items:1,
-                nav:true
+                // dots:true,
+                margin:18
             },
-            600:{
-                items:4,
-                nav:false
+            768:{
+                margin:30
+               
             }
         }
     });
     $(".faq_item_js").click(function(){
         $(this).find(".switcher").toggleClass("opened").parents(".faq_item").find(".faq_item_content").slideToggle("fast");
     });
+    $(".menu_mob_js").click(function(){
+        $(this).toggleClass("opened");
+        $(".nav_holder_js").toggleClass("opened");
+        $("body").toggleClass("menu_opened");
+    });
+    // $('a[href^="#"]').on('click', function (e) {
+    //     e.preventDefault();
+      
+    //     var targetEle = this.hash;
+    //     var $targetEle = $(targetEle);
+      
+    //     $('html, body').stop().animate({
+    //         'scrollTop': $targetEle.offset().top
+    //     }, 800, 'swing', function () {
+    //         window.location.hash = targetEle;
+    //     });
+    // });
 });
